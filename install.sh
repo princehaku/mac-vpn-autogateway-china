a350e270ffa9d2eddb80ef49b89ffa503948f4ea
zhongwei.bzw <zhongwei.bzw@taobao.com>
2014-03-11 21:13:16 +0800

    new init version

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "MAC VPN AUTOGATEWAY "
+echo "by http://3haku.net"
+if [[ ! $(id -u) -eq 0 ]] ; then
+	echo "You Must Be Root"
+	exit 0
+fi
+
+cp -r ./* /etc/ppp/
+echo "   ALL DONE   "
